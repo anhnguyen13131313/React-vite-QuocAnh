2f13bb82763179feb627ba37b70eec4b36e2322d
Quoc Anh <quocanhnguyen1313@gmail.com>
2026-09-14 14:25:37 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
+  // function myFunction() {
+  //   console.log("my function");
+  // }
+  let myFunction = () => {
+    console.log("my function");
+  };
   return (
     <>
       <div>
@@ -30,6 +36,6 @@ function App() {
       </p>
     </>
   );
-}
+};
 
 export default App;

813b2407f03d981cb883f5efa7aecb1a8f11c5ae
Quoc Anh <quocanhnguyen1313@gmail.com>
2026-09-15 13:10:12 +0700

    video 31

--- src/Components/learn/MyComponent.jsx
+++ src/Components/learn/MyComponent.jsx
@@ -2,9 +2,21 @@
 //fragment
 import "./style.css";
 const MyComponent = () => {
+  // const hoidanit = "eric 1";
+  // const hoidanit = 25;
+  // const hoidanit = true;
+  // const hoidanit = undefined;
+  // const hoidanit = null;
+  const hoidanit = [1, 2, 3];
+  // const hoidanit = {
+  //   name: "hoidanit",
+  //   age: 25,
+  // };
+
   return (
     <>
-      <div>eric & hoidanit update</div>
+      <div>{JSON.stringify(hoidanit)} & hoidanit update</div>
+      <div>{console.log("eric")}</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         child
       </div>

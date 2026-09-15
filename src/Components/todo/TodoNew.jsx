a8997f8ce3adef61da55ff621934c6d87e45fd32
Quoc Anh <quocanhnguyen1313@gmail.com>
2026-09-15 17:55:04 +0700

    video 35

--- src/Components/todo/TodoNew.jsx
+++ src/Components/todo/TodoNew.jsx
@@ -1,8 +1,13 @@
-const TodoNew = () => {
-    return (<div className="todo-new">
-        <input type="text" />
-        <button>Add</button>
-      </div>)
-}
+const TodoNew = (props) => {
+  const { addNewTodo } = props;
+
+//   addNewTodo("eric");
+  return (
+    <div className="todo-new">
+      <input type="text" />
+      <button>Add</button>
+    </div>
+  );
+};
 
 export default TodoNew;

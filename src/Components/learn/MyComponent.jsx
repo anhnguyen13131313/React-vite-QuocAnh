9447467d9f3ac613d29bad51c0861b0c3a75e113
Quoc Anh <quocanhnguyen1313@gmail.com>
2026-09-15 12:58:25 +0700

    video 30

--- src/Components/learn/MyComponent.jsx
+++ src/Components/learn/MyComponent.jsx
@@ -1,10 +1,15 @@
-const FakeComponent = () => {
-  return <div>Fake component</div>;
-};
-
 // component = html + css + js
+//fragment
+import "./style.css";
 const MyComponent = () => {
-  return <div>eric & hoidanit update</div>;
+  return (
+    <>
+      <div>eric & hoidanit update</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        child
+      </div>
+    </>
+  );
 };
 
 export default MyComponent;

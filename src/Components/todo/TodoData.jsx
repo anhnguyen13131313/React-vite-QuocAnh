f7112c301efeacb58df4deb21f2bde99b530c0c0
Quoc Anh <quocanhnguyen1313@gmail.com>
2026-09-15 16:27:11 +0700

    video 34

---
+++ src/Components/todo/TodoData.jsx
@@ -0,0 +1,23 @@
+const TodoData = (props) => {
+  //props la mot bien object{}
+  // {
+  //     name: "eric",
+  //     age: 25,
+  //     data;{}
+  // }
+    const { name, age, data } = props;
+  //   const name = props.name;
+  //   const age = props.age;
+  //   const data = props.data;
+
+//   console.log(props);
+  return (
+    <div className="todo-data">
+      <div>My name is: {name}</div>
+      <div>Learning React</div>
+      <div>Watching Youtube</div>
+    </div>
+  );
+};
+
+export default TodoData;

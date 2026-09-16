3ca2758c8a0847123f61c107d9cae0ac97174eeb
Quoc Anh <quocanhnguyen1313@gmail.com>
2026-09-16 12:01:53 +0700

    video 40

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,9 @@ import reactLogo from "./assets/react.svg";
 import { useState } from "react";
 const App = () => {
   const [todoList, setTodoList] = useState([
-    { id: 1, name: "Learning React" },
-    { id: 2, name: "Watching Youtube" },
+    // { id: 1, name: "Learning React" },
+    // { id: 2, name: "Watching Youtube" },
   ]);
-  const hoidanit = "eric Arsenal";
-  const age = 25;
-  const data = {
-    address: "hanoi",
-    country: "vietnam",
-  };
   const addNewTodo = (name) => {
     const newTodo = {
       id: randomIntFromInterval(1, 10000),
@@ -29,7 +23,7 @@ const App = () => {
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew addNewTodo={addNewTodo} />
-      <TodoData name={hoidanit} age={age} data={data} todoList={todoList} />
+      <TodoData todoList={todoList} />
       <div className="todo-image">
         <img src={reactLogo} />
       </div>

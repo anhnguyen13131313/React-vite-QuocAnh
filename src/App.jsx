006757a324634112a6231e8a900b6dac86968562
Quoc Anh <quocanhnguyen1313@gmail.com>
2026-09-16 10:57:46 +0700

    video 38

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import TodoNew from "./Components/todo/TodoNew";
 import reactLogo from "./assets/react.svg";
 import { useState } from "react";
 const App = () => {
-  const [todoList, setTodoList]= useState([
-    {id: 1, name: "Learning React"},
-    {id: 2, name: "Watching Youtube"}
-  ])
+  const [todoList, setTodoList] = useState([
+    { id: 1, name: "Learning React" },
+    { id: 2, name: "Watching Youtube" },
+  ]);
   const hoidanit = "eric Arsenal";
   const age = 25;
   const data = {
@@ -15,13 +15,21 @@ const App = () => {
     country: "vietnam",
   };
   const addNewTodo = (name) => {
-    alert(`call me: ${name}`)
+    const newTodo = {
+      id: randomIntFromInterval(1, 10000),
+      name: name,
+    };
+    setTodoList([...todoList, newTodo]);
+  };
+  const randomIntFromInterval = (min, max) => {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
   };
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew addNewTodo={addNewTodo} />
-      <TodoData name={hoidanit} age={age} data={data} todoList = {todoList} />
+      <TodoData name={hoidanit} age={age} data={data} todoList={todoList} />
       <div className="todo-image">
         <img src={reactLogo} />
       </div>
--- src/Components/todo/TodoNew.jsx
+++ src/Components/todo/TodoNew.jsx
@@ -9,7 +9,7 @@ const TodoNew = (props) => {
 
   //   addNewTodo("eric");
   const handleClick = () => {
-    console.log("check valueInput", valueInput)
+    addNewTodo(valueInput)
   };
 
   const handleOnChange = (name) => {

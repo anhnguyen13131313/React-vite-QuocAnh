5f6ba7bcf537a0f183a9b1f04ea085bb3621847a
Quoc Anh <quocanhnguyen1313@gmail.com>
2026-09-16 18:59:34 +0700

    video 48

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import reactLogo from "./assets/react.svg";
 import Header from "./Components/layout/header";
 import Footer from "./Components/layout/footer";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 const App = () => {
   const [todoList, setTodoList] = useState([
     // { id: 1, name: "Learning React" },
@@ -41,6 +42,7 @@ const App = () => {
           </div>
         )}
       </div>
+      <Outlet />
       <Footer />
     </>
   );
